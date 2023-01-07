62763e077271b6956be0b614d1c41b89a1d4e03e
pravin khandare <100687226+pravinKhandare007@users.noreply.github.com>
2023-01-07 08:36:20 +0530

    Add files via upload

--- Exploring_toString_Method.java
+++ Exploring_toString_Method.java
@@ -1,7 +1,16 @@
 package daily.string;
 
+class Gg{
+	int g;
+
+	@Override
+	public String toString() {
+		return "Gg [g=" + g + "]";
+	}
+}
+
+
 public class Exploring_toString_Method {
-	
 	
 	@Override
 	public String toString() {
@@ -13,5 +22,15 @@ public class Exploring_toString_Method {
 		Exploring_toString_Method ex = new Exploring_toString_Method();
 		System.out.println(a);
 		System.out.println(ex);
+		
+		String st = "what will the st return in sysout";
+		System.out.println(st);
+		char[] ch = {'a','b','b'};
+		
+		String st2 = new String(ch);
+		System.out.println(st2);
+		
+		Gg g= new Gg();
+		System.out.println(g);
 	}
 }
