57ce84f174d40c901c6ec40b2c60cc7d9005981e
pravin khandare <100687226+pravinKhandare007@users.noreply.github.com>
2023-01-17 23:26:14 +0530

    Add files via upload

--- StringRotation.java
+++ StringRotation.java
@@ -5,18 +5,28 @@ public class StringRotation {
 	public String rotateString(String s) {
 		char[] charArrayOfString = s.toCharArray();
 		char firstCharecter = charArrayOfString[0];
-		for(int i =0; i < charArrayOfString.length;i++ ) {
+		for(int i =0; i < charArrayOfString.length - 1;i++ ) {
 			charArrayOfString[i] = charArrayOfString[i+1];
 		}
+		charArrayOfString[charArrayOfString.length - 1]=firstCharecter;
+		String rotatedString = new String(charArrayOfString);
 		return rotatedString;
 	}
 
 	public static void main(String[] args) {
-		String taget = "cdeab";
+		String target = "cdeab";
 		String input = "abcde";
 		
+		boolean areInputAndTargetEqualAfterRotation = false;
 		StringRotation cs = new StringRotation();
-		String rotatedString = cs.rotateString(input);
+		for(int i =0; i < input.length();i++) {
+			input = cs.rotateString(input);
+			System.out.println(input);
+			if(target.equals(input)){
+				areInputAndTargetEqualAfterRotation = true;
+			}
+		}
+		
 		
 
 	}
