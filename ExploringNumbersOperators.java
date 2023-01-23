4773577ce0fc2e28394366181be4b2a79778e1df
pravin khandare <100687226+pravinKhandare007@users.noreply.github.com>
2023-01-23 22:49:06 +0530

    Add files via upload

---
+++ ExploringNumbersOperators.java
@@ -0,0 +1,11 @@
+package daily.misselenious;
+
+public class ExploringNumbersOperators {
+	public static void main(String[] args) {
+		int a = 159;
+		System.out.println(a/100);
+		System.out.println(a%100);
+		System.out.println(a/10);
+		System.out.println(a%10);
+	}
+}
